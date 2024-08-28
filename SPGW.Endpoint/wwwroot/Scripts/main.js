5cbd72d7aa02e8c40e7e23774ee6a6a8bf6c642d
Yasin Abedini <yasinabedini1384@gmail.com>
2024-08-28 21:28:56 +0330

    refactor - Change and Fix project Path

--- SPGW.Endpoint/wwwroot/Scripts/main.js
+++
@@ -1,5 +0,0 @@
-$(()=>{
-  if($('col-brand > brand').length < 4){
-    $('container > column > col-body col-brand').css('grid-template-columns', `repeat(${$('col-brand > brand').length}, 1fr)`)
-  }
-})
