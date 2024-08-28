927ebbbc8b0f061fdb358dbd994da8994c1a7766
Yasin Abedini <yasinabedini1384@gmail.com>
2024-08-28 12:10:40 +0330

    fix - fix some bugs

---
+++ Src/Endpoints/SPGW.Endpoint/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using SPGW.Infra.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SPGWContext>(options =>
+      options.UseSqlServer(builder.Configuration.GetConnectionString("LOCAL_DB"))
+             .UseLazyLoadingProxies()); // If you need lazy loading proxies
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
